4a37aae91fb5615689d18ff065aa113b7bc692f6
dnttalk <126428629+dnttalk@users.noreply.github.com>
2023-08-29 09:49:21 +0800

    1
    1

--- public/assets/js/secondPage.js
+++ public/assets/js/secondPage.js
@@ -69,33 +69,6 @@ const backspace = document.getElementById('backspace');
 const clear = document.getElementById('clear');
 const ok = document.getElementById('ok');
 
-// sampleNumberInput.addEventListener('click', () => {
-//     keyboard.style.display = 'grid';
-//     const inputValue = parseInt(sampleNumberInput.value);
-
-//     // 取得所有的 tdFirst 和 tdSecond 元素
-//     const tdFirstElements = document.querySelectorAll("[id^='tdFirst_']");
-//     const tdSecondElements = document.querySelectorAll("[id^='tdSecond_']");
-
-//     // 移除所有單元格的綠色樣式
-//     tdFirstElements.forEach(td => td.classList.remove("green"));
-//     tdSecondElements.forEach(td => td.classList.remove("green"));
-
-//     // 將對應數量的單元格設置為綠色
-//     for (let i = 1; i <= inputValue; i++) {
-//         const tdFirst = document.getElementById(`tdFirst_${i}`);
-//         const tdSecond = document.getElementById(`tdSecond_${i}`);
-
-//         if (tdFirst) {
-//             tdFirst.classList.add("green");
-//         }
-
-//         if (tdSecond) {
-//             tdSecond.classList.add("green");
-//         }
-//     }
-// });
-
 let updateSscircleBtnEvent = function () {
     for (let i = 1; i <= 24; i++) {
         if (i <= sampleNumberInput.value) {
